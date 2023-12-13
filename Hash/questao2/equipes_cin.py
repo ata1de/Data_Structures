28989f566eebaa6db5ea1e42746ed3fcd49b85f8
unknown <teteulima@outlook.com>
2023-12-13 10:26:23 -0300

    started new question

---
+++ Hash/questao2/equipes_cin.py
@@ -0,0 +1,20 @@
+class Elemento:
+  def __init__(self,valor, tamanho):
+    self.valor = valor
+    self.next = None
+    self.tamanho = tamanho
+
+    
+
+
+
+while True:
+    try:
+        pessoa, tamanho_grupo = input().split() 
+    except EOFError:
+       break
+
+
+
+
+    
